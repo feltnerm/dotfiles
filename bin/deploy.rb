deeaf219b536300ecbebbfe63a825b38aa9f6bda
Mark Feltner <feltner.mj@gmail.com>
2012-11-23 01:04:27 -0600

    commented

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
-
+##
+# Deploy.rb
+#
+# Deploy dotfiles into users $HOME via symlinks
+# Useful for those who keep a version controlled (via git) folder
+# full of dotfiles
+#
+# notes: 
+#   does not symlink bin/
+#   does not symlink .git or .gitignore (for obvious reasons)
+##
 
 def main
 
@@ -8,16 +18,19 @@ def main
     
     Dir.foreach(dotfiles_dir) { |x| 
 
+        # where the original file resides
         from = File.join(dotfiles_dir, x)
 
         if not (x == "." or x == ".." or x == ".git" or x == ".gitignore")
 
+            # where to symlink will belong 
             if x != "bin"
                 to = File.join(home, "."+x)
             else
                 to = File.join(home, x)
             end
 
+            # the meat:
             if not File.exists?(to) and not File.symlink?(to)
                 puts "#{from} ==> #{to}"
                 File.symlink(from, to)
@@ -34,3 +47,4 @@ if __FILE__ == $0
     main
 
 end
+
