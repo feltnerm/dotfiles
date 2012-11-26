02f3bfb2fecd4f01c934384d0877142c10014376
Mark Feltner <feltner.mj@gmail.com>
2012-11-26 10:37:01 -0600

    new tags

--- config/awesome/rc/tags.lua
+++ config/awesome/rc/tags.lua
@@ -1,7 +1,19 @@
 -- Tags
 local tags = {}
 --local tagnames = { "⌨", "⍜", "✉", "❁", "✣" }
-local tagnames = { "⌨", "❁", "✣" }
+--local tagnames = { "⌨", "❁", "✣" }
+local tagnames = {"☕", "⌨", "⌘", "☭"}
+
+if false then
+    local screens = screen.count()
+    if screens == 1 then
+        tags[1] = awful.tag(screen1_tagnames, 1, config.layouts[1])
+    elseif screens == 2 then
+        tags[1] = awful.tag(screen1_tagnames, 1, config.layouts[1])
+        tags[2] = awful.tag(screen2_tagnames, 2, config.layouts[1])
+    end
+end
+
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     tags[s] = awful.tag(tagnames, s, config.layouts[1])
