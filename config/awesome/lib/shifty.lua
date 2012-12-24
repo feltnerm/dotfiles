98f8bcc82974321f080ccfb691b3782913708680
Mark Feltner <feltner.mj@gmail.com>
2012-12-23 22:40:17 -0600

    moving to submodule

--- config/awesome/lib/shifty.lua
+++
@@ -1,4 +0,0 @@
-require("shifty/shifty")
-shifty.config.taglist = mytaglist -- for some reason we need to specify the taglist to shifty...
-
--- vim: filetype=lua:expandtab:shiftwidth=2:tabstop=2:softtabstop=2:textwidth=80
