cfcbe4d41c39f0b664947d3d7682c51a05b2dbf2
Mark Feltner <mfeltner@widen.com>
2016-08-15 22:45:54 -0500

    fix(go.fish): path no longer needed

--- dotfiles/config/fish/lib/go.fish
+++ dotfiles/config/fish/lib/go.fish
@@ -1,6 +1,6 @@
-set --global --export GOPATH "$HOME/.go"
+# set --global --export GOPATH "$HOME/.go"
 
 # _is_osx && export GOROOT="$(brew --prefix go)/libexec"
 
-set --global --export PATH $GOROOT/bin $PATH
-set --global --export PATH $GOPATH/bin $PATH
+# set --global --export PATH $GOROOT/bin $PATH
+# set --global --export PATH $GOPATH/bin $PATH
