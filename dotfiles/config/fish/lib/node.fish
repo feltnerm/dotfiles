a802e0446a093b388351ebc48873e4c0df59309b
Mark Feltner <mfeltner@widen.com>
2016-08-15 22:44:57 -0500

    fix(node.fish): path no longer needed

--- dotfiles/config/fish/lib/node.fish
+++ dotfiles/config/fish/lib/node.fish
@@ -1,6 +1,7 @@
-set --global --export NPM_PACKAGES "$HOME/.npm-packages"
-set --global --export NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-set --global --export PATH "$NPM_PACKAGES/bin" $PATH
+# @todo: deprecated by use of nvm ... ?
+# set --global --export NPM_PACKAGES "$HOME/.npm-packages"
+# set --global --export NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+# set --global --export PATH "$NPM_PACKAGES/bin" $PATH
 
 # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
 # unset MANPATH # delete if you already modified MANPATH elsewhere in your config
