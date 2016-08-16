77fb79267623777c10d3e8125844c1ecdb717164
Mark Feltner <mfeltner@widen.com>
2016-08-15 23:04:37 -0500

    fix(tmux): invalid settings and aliases converted to fish

--- dotfiles/config/fish/functions/tmuxa.fish
+++ dotfiles/config/fish/functions/tmuxa.fish
@@ -1,4 +1,4 @@
 function tmuxa
-  [[ -z $1 ]] && tmux attach -t (basename (pwd))
-  [[ -n $1 ]] && tmux attach -t $1
+  [[ -z $1 ]]; and tmux attach -t (basename (pwd))
+  [[ -n $1 ]]; and tmux attach -t $1
 end
--- dotfiles/config/fish/functions/tmuxn.fish
+++ dotfiles/config/fish/functions/tmuxn.fish
@@ -1,4 +1,4 @@
 function tmuxn
-  [[ -z $1 ]] && tmux new-session -s (basename (pwd))
-  [[ -n $1 ]] && tmux new-session -s $1
+  [[ -z $1 ]]; and tmux new-session -s (basename (pwd))
+  [[ -n $1 ]]; tmux new-session -s $1
 end
