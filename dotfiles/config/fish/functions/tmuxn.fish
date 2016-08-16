0e482be74f5151e742332229023ddc2b86ef1b48
Mark Feltner <mfeltner@widen.com>
2016-08-15 23:05:42 -0500

    fix(tmux): aliases converted to fish

--- dotfiles/config/fish/functions/tmuxn.fish
+++ dotfiles/config/fish/functions/tmuxn.fish
@@ -1,4 +1,4 @@
 function tmuxn
-  [[ -z $1 ]]; and tmux new-session -s (basename (pwd))
-  [[ -n $1 ]]; tmux new-session -s $1
+  eval [[ -z $1 ]]; and tmux new-session -s (basename (pwd))
+  eval [[ -n $1 ]]; and tmux new-session -s $1
 end
