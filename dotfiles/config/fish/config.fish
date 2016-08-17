9d78e23afa9073bddff180fe35ca2a78c22b57bf
Mark Feltner <feltner.mj@gmail.com>
2016-08-17 02:03:39 -0500

    fix(config.fish): set EDITOR

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,10 +1,11 @@
-# Path to fish config 
+# Path to fish config
 set --global --export FISH_HOME "$HOME/.config/fish"
 
 # Path to Oh My Fish install.
 set --global --export OMF_PATH "$HOME/.local/share/omf"
 
 source "$FISH_HOME/lib/init.fish"
+set --global --export EDITOR nvim
 
 # source "$HOME/.extras.fish"
 # Load oh-my-fish configuration.
