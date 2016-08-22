930a16d4af3a43a41bcf7d370d7c0ad66a796178
Mark Feltner <feltner.mj@gmail.com>
2016-08-22 01:26:30 -0500

    feat(fish/ssh-agent): use ssh-agent with fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -5,7 +5,38 @@ set --global --export FISH_HOME "$HOME/.config/fish"
 set --global --export OMF_PATH "$HOME/.local/share/omf"
 
 source "$FISH_HOME/lib/init.fish"
+#
+# Editor
+#
 set --global --export EDITOR nvim
+
+#
+# SSH Agent
+#
+setenv SSH_ENV $HOME/.ssh/environment
+
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+        source $SSH_ENV > /dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
+    end
+end
+
+#
+# Virtualfish
+#
+eval (python -m virtualfish)
+
 
 # source "$HOME/.extras.fish"
 # Load oh-my-fish configuration.
