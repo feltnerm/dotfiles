bdce5a0be939116c79f6c65e0384ce3d2268d4cc
Mark Feltner <mfeltner@widen.com>
2017-08-22 11:53:55 -0500

    fix(fish/functions): tmuxa

--- dotfiles/config/fish/functions/tmuxa.fish
+++ dotfiles/config/fish/functions/tmuxa.fish
@@ -1,4 +1,4 @@
 function tmuxa
-  eval [[ -z $1 ]]; and tmux attach -t (basename (pwd))
-  eval [[ -n $1 ]]; and tmux attach -t $1
+  eval [[ -z $1 ]]; and tmux attach-session -t (basename (pwd))
+  eval [[ -n $1 ]]; and tmux attach-session -t $1
 end
