0633a55ea5620d72fded9cce55bdb09eb69f3a9c
Mark Feltner <mfeltner@widen.com>
2017-12-26 10:20:28 -0600

    fix(fish/tmuxa): correctly attach to session

--- dotfiles/config/fish/functions/tmuxa.fish
+++ dotfiles/config/fish/functions/tmuxa.fish
@@ -1,4 +1,7 @@
 function tmuxa
-  eval [[ -z $1 ]]; and tmux attach-session -t (basename (pwd))
-  eval [[ -n $1 ]]; and tmux attach-session -t $1
+    if count $argv > /dev/null
+        tmux attach-session -t $argv
+    else
+        tmux attach-session -t (basename (pwd))
+    end
 end
