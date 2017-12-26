3fb6ffb78be241566d14607b0d4d539282d5375e
Mark Feltner <mfeltner@widen.com>
2017-12-26 10:21:46 -0600

    style(fish/go): set GOPATH and add to PATH

--- dotfiles/config/fish/lib/go.fish
+++ dotfiles/config/fish/lib/go.fish
@@ -1,6 +1,5 @@
-# set --global --export GOPATH "$HOME/.go"
+set --global --export GOPATH "$HOME/.go"
+set --global --export PATH $GOPATH/bin $PATH
 
 # _is_osx && export GOROOT="$(brew --prefix go)/libexec"
-
 # set --global --export PATH $GOROOT/bin $PATH
-# set --global --export PATH $GOPATH/bin $PATH
