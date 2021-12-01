d01d0c1ae367d9f4df96a01e68cd91c2643b57ff
Mark Feltner <feltner.mj@gmail.com>
2021-12-01 08:48:58 -0600

    fix(lein/profiles): and simplify

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -1,34 +1,39 @@
-{:user {:plugins []
-        :dependencies []
-        :injections [(require 'com.georgejahad.difform)
-                     (require 'alex-and-georges.debug-repl)
-                     (require '[vinyasa.inject :as inject])
-                     (inject/in
-                      ;; note that `:refer, :all and :exclude can be used
-                      [vinyasa.inject :refer [inject [in inject-in]]]
-                      [vinyasa.lein :exclude [*project*]]
+{:repl {:dependencies [[alembic "0.3.2"]]}
+ ;; :dev {:dependencies []
+ ;;       :source-paths ["env/dev"]    ;additional source path
+ ;;       :main user/-main             ;which namespace and function to start in
+       }
+ :user {:plugins [[io.aviso/pretty "1.1"]       ;make stack traces pretty
+                  [jonase/eastwood "0.9.9"]     ; more linting
+                  [lein-ancient "1.0.0-RC3"]    ;show out of date dependencies
+                  [lein-bikeshed "0.5.2"]       ; linting
+                  [lein-cljfmt "0.8.0"]         ; code prettification
+                  [lein-kibit "0.1.8"]          ; make code more idiomatic
+                  [lein-pprint "1.3.2"]         ;pretty print lein dependencies
+                  [lein-try "0.4.3"]            ;easy to try new libraries
+                  [metosin/bat-test "0.4.4"]    ;test runner
+                  [mvxcvi/whidbey "2.2.1"]      ;repl pretty-printing
 
-                      ;; imports all functions in vinyasa.pull
-                      [alembic.still [distill pull]]
-
-                      clojure.core
-                      [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-
-                      clojure.core >
-                      ;; [clojure.java.io :as io]
-                      ;; [clojure.set :as set]
-                      ;; [clojure.string :as str]
-                      ;; [clojure.test :as test]
-                      ;; [clojure.repl :refer [apropos dir doc find-doc pst source]]
-                      [clojure.java.javadoc :refer [javadoc]]
-                      [clojure.reflect :refer [reflect]]
-                      [clojure.pprint pprint]
-                      [clojure.java.shell sh]
-                      [com.georgejahad.difform difform]
-                      [alex-and-georges.debug-repl debug-repl])]
+                  ;; vinyasa tools for REPL-coding
+                  ;; [im.chit/vinyasa.pull "0.2.0"]
+                  [im.chit/vinyasa.inject "0.2.0"]
+                  [im.chit/vinyasa.lein "0.2.0"]
+                  [im.chit/vinyasa.reimport "0.2.0"]]
+        :middleware [;; io.aviso.lein-pretty/inject   ;starts pretty during REPL start-up
+                     whidbey.plugin/repl-pprint]
+        :dependencies [[com.bhauman/rebel-readline "0.1.4"]     ;nice REPL
+                       [expectations/clojure-test "1.2.1"]      ;library for testing
+                       [expound "0.8.10"]                       ;improve error messages
+                       [io.aviso/pretty "1.1"]                  ;formatting of exceptions
+                       [mvxcvi/puget "1.3.1"]                   ;colour print data
+                       [org.clojure/tools.namespace "1.1.0"]    ;reload
+                       ]
+        :injections [;colours for stack traces in the REPL
+                     (require 'io.aviso.repl)]
         :aliases {"omni" ["do"
                           ["clean"]
                           ["with-profile" "production" "deps" ":tree"]
                           ["ancient"]
                           ["kibit"]
-                          ["bikeshed"]]}}}
+                          ["bikeshed"]]
+                  "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}}
